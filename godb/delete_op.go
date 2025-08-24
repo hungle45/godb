9e3c25c2fc453e64ad544da1ac0dc8f6e5807058
hungln <hungln.work@gmail.com>
2025-08-24 22:52:47 +0700

    feat: impl delete & insert operator

--- godb/delete_op.go
+++ godb/delete_op.go
@@ -5,29 +5,53 @@ import (
 )
 
 type DeleteOp struct {
-	// TODO: some code goes here
+	deleteFile DBFile
+	child      Operator
 }
 
-// Construct a delete operator. The delete operator deletes the records in the
+// NewDeleteOp Construct a delete operator. The delete operator deletes the records in the
 // child Operator from the specified DBFile.
 func NewDeleteOp(deleteFile DBFile, child Operator) *DeleteOp {
-	// TODO: some code goes here
-	return nil // replace me
+	return &DeleteOp{deleteFile: deleteFile, child: child}
 }
 
-// The delete TupleDesc is a one column descriptor with an integer field named
+// Descriptor To delete TupleDesc is a one column descriptor with an integer field named
 // "count".
 func (i *DeleteOp) Descriptor() *TupleDesc {
-	// TODO: some code goes here
-	return &TupleDesc{} // replace me
-
+	return &TupleDesc{Fields: []FieldType{{Fname: "count", Ftype: IntType}}}
 }
 
-// Return an iterator that deletes all of the tuples from the child iterator
+// Iterator Return an iterator that deletes all the tuples from the child iterator
 // from the DBFile passed to the constructor and then returns a one-field tuple
 // with a "count" field indicating the number of tuples that were deleted.
 // Tuples should be deleted using the [DBFile.deleteTuple] method.
 func (dop *DeleteOp) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
-	// TODO: some code goes here
-	return nil, fmt.Errorf("DeleteOp.Iterator not implemented") // replace me
+	childIter, err := dop.child.Iterator(tid)
+	if err != nil {
+		return nil, err
+	}
+
+	return func() (*Tuple, error) {
+		var count int64
+		for {
+			tuple, err := childIter()
+			if err != nil {
+				return nil, fmt.Errorf("DeleteOp.Iterator: %w", err)
+			}
+			if tuple == nil {
+				break
+			}
+
+			err = dop.deleteFile.deleteTuple(tuple, tid)
+			if err != nil {
+				return nil, fmt.Errorf("DeleteOp.Iterator: %w", err)
+			}
+			count++
+		}
+
+		return &Tuple{
+			Desc:   *dop.Descriptor(),
+			Fields: []DBValue{IntField{count}},
+		}, nil
+	}, nil
 }
--- godb/insert_op.go
+++ godb/insert_op.go
@@ -3,28 +3,53 @@ package godb
 import "fmt"
 
 type InsertOp struct {
-	// TODO: some code goes here
+	insertFile DBFile
+	child      Operator
 }
 
-// Construct an insert operator that inserts the records in the child Operator
+// NewInsertOp Construct an insert operator that inserts the records in the child Operator
 // into the specified DBFile.
 func NewInsertOp(insertFile DBFile, child Operator) *InsertOp {
-	// TODO: some code goes here
-	return nil
+	return &InsertOp{insertFile: insertFile, child: child}
 }
 
-// The insert TupleDesc is a one column descriptor with an integer field named "count"
+// Descriptor The insert TupleDesc is a one column descriptor with an integer field named "count"
 func (i *InsertOp) Descriptor() *TupleDesc {
-	// TODO: some code goes here
-	return nil
+	return &TupleDesc{Fields: []FieldType{{Fname: "count", Ftype: IntType}}}
 }
 
-// Return an iterator function that inserts all of the tuples from the child
+// Iterator Return an iterator function that inserts all of the tuples from the child
 // iterator into the DBFile passed to the constuctor and then returns a
 // one-field tuple with a "count" field indicating the number of tuples that
 // were inserted.  Tuples should be inserted using the [DBFile.insertTuple]
 // method.
 func (iop *InsertOp) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
-	// TODO: some code goes here
-	return nil, fmt.Errorf("InsertOp.Iterator not implemented")
+	childIter, err := iop.child.Iterator(tid)
+	if err != nil {
+		return nil, err
+	}
+
+	return func() (*Tuple, error) {
+		var count int64
+		for {
+			tuple, err := childIter()
+			if err != nil {
+				return nil, fmt.Errorf("InsertOp.Iterator: %w", err)
+			}
+			if tuple == nil {
+				break
+			}
+
+			err = iop.insertFile.insertTuple(tuple, tid)
+			if err != nil {
+				return nil, fmt.Errorf("InsertOp.Iterator: %w", err)
+			}
+			count++
+		}
+
+		return &Tuple{
+			Desc:   *iop.Descriptor(),
+			Fields: []DBValue{IntField{count}},
+		}, nil
+	}, nil
 }
