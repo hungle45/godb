6956b3ad46e9f7dc4e9b05980e24851df8e9edad
hungln <hungln.work@gmail.com>
2025-08-25 13:24:32 +0700

    feat: impl order by operator

--- godb/order_by_op.go
+++ godb/order_by_op.go
@@ -2,35 +2,43 @@ package godb
 
 import (
 	"fmt"
+	"sort"
 )
 
 type OrderBy struct {
 	orderBy []Expr // OrderBy should include these two fields (used by parser)
 	child   Operator
-	// TODO: You may want to add additional fields here
+	asc     []bool // true for ascending, false for descending
 }
 
-// Construct an order by operator. Saves the list of field, child, and ascending
+// NewOrderBy Construct an order by operator. Saves the list of field, child, and ascending
 // values for use in the Iterator() method. Here, orderByFields is a list of
 // expressions that can be extracted from the child operator's tuples, and the
 // ascending bitmap indicates whether the ith field in the orderByFields list
 // should be in ascending (true) or descending (false) order.
 func NewOrderBy(orderByFields []Expr, child Operator, ascending []bool) (*OrderBy, error) {
-	// TODO: some code goes here
-	return nil, fmt.Errorf("NewOrderBy not implemented.") //replace me
-
+	if len(orderByFields) == 0 {
+		return nil, fmt.Errorf("empty order by fields")
+	}
+	if len(orderByFields) != len(ascending) {
+		return nil, fmt.Errorf("mismatched lengths for order by fields and ascending list")
+	}
+	return &OrderBy{
+		orderBy: orderByFields,
+		child:   child,
+		asc:     ascending,
+	}, nil
 }
 
-// Return the tuple descriptor.
+// Descriptor Return the tuple descriptor.
 //
 // Note that the order by just changes the order of the child tuples, not the
 // fields that are emitted.
 func (o *OrderBy) Descriptor() *TupleDesc {
-	// TODO: some code goes here
-	return &TupleDesc{} // replace me
+	return o.child.Descriptor()
 }
 
-// Return a function that iterates through the results of the child iterator in
+// Iterator Return a function that iterates through the results of the child iterator in
 // ascending/descending order, as specified in the constructor.  This sort is
 // "blocking" -- it should first construct an in-memory sorted list of results
 // to return, and then iterate through them one by one on each subsequent
@@ -43,6 +51,101 @@ func (o *OrderBy) Descriptor() *TupleDesc {
 // example, example of SortMultiKeys, and documentation at:
 // https://pkg.go.dev/sort
 func (o *OrderBy) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
-	// TODO: some code goes here
-	return nil, fmt.Errorf("OrderBy.Iterator not implemented") // replace me
+	childIter, err := o.child.Iterator(tid)
+	if err != nil {
+		return nil, err
+	}
+	var (
+		tuples []*Tuple
+		index  int
+		init   bool
+	)
+
+	return func() (*Tuple, error) {
+		if !init {
+			for {
+				tuple, err := childIter()
+				if err != nil {
+					return nil, err
+				}
+				if tuple == nil {
+					break
+				}
+				tuples = append(tuples, tuple)
+			}
+
+			sorter, err := newTupleSorter(tuples, o.orderBy, o.asc)
+			if err != nil {
+				return nil, err
+			}
+			sort.Sort(sorter)
+			tuples = sorter.tuples
+			init = true
+		}
+
+		if index >= len(tuples) {
+			return nil, nil
+		}
+		tuple := tuples[index]
+		index++
+		return tuple, nil
+	}, nil
+}
+
+type tupleSorter struct {
+	tuples    []*Tuple
+	orderBy   []Expr
+	cacheExpr [][]DBValue
+	asc       []bool
+}
+
+func newTupleSorter(tuples []*Tuple, orderBy []Expr, asc []bool) (*tupleSorter, error) {
+	if len(orderBy) == 0 {
+		return nil, fmt.Errorf("empty order by fields")
+	}
+	if len(orderBy) != len(asc) {
+		return nil, fmt.Errorf("mismatched lengths for order by fields and ascending list")
+	}
+
+	cacheExpr := make([][]DBValue, len(tuples))
+	for i, tuple := range tuples {
+		cacheExpr[i] = make([]DBValue, len(orderBy))
+		for j, expr := range orderBy {
+			val, err := expr.EvalExpr(tuple)
+			if err != nil {
+				return nil, err
+			}
+			cacheExpr[i][j] = val
+		}
+	}
+
+	return &tupleSorter{
+		tuples:    tuples,
+		orderBy:   orderBy,
+		cacheExpr: cacheExpr,
+		asc:       asc,
+	}, nil
+}
+
+func (ts *tupleSorter) Len() int {
+	return len(ts.tuples)
+}
+
+func (ts *tupleSorter) Swap(i, j int) {
+	ts.tuples[i], ts.tuples[j] = ts.tuples[j], ts.tuples[i]
+	ts.cacheExpr[i], ts.cacheExpr[j] = ts.cacheExpr[j], ts.cacheExpr[i]
+}
+
+func (ts *tupleSorter) Less(i, j int) bool {
+	for k := 0; k < len(ts.orderBy); k++ {
+		valI := ts.cacheExpr[i][k]
+		valJ := ts.cacheExpr[j][k]
+
+		if valI.EvalPred(valJ, OpLt) {
+			return ts.asc[k]
+		} else if valI.EvalPred(valJ, OpGt) {
+			return !ts.asc[k]
+		}
+	}
+	return false
 }
